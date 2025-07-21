68f52a0c8c3af2f2f10a6179f8fc136978e8d37a
sindhavdinesh <sindhavdinesh93@gmail.com>
2025-07-21 12:40:28 +0530

    Add files via upload

---
+++ project_1.2 .py
@@ -0,0 +1,662 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the pattern Generator and number Analyzer!
+
+Select an option.
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a range of Numbers
+
+Enter your pattern: 6
+Invalid choice.
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the pattern Generator and number Analyzer!
+
+Select an option.
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a range of Numbers
+
+Enter your pattern: 10
+Invalid choice.
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the pattern Generator and number Analyzer!
+
+Select an option.
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a range of Numbers
+
+Enter your pattern: 4
+Invalid choice.
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the pattern Generator and number Analyzer!
+
+Select an option.
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a range of Numbers
+
+Enter your pattern: 1
+Enter the number of rows for the pattern: 2
+Pattern:
+*
+**
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the pattern Generator and number Analyzer!
+
+Select an option.
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a range of Numbers
+
+Enter your pattern: 1
+Enter the number of rows for the pattern: 9
+Pattern:
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the pattern Generator and number Analyzer!
+
+Select an option:
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a range of Number
+
+Enter your pattern: 1
+Enter the number of rows for the pattern: 4
+Pattern:
+*
+**
+***
+****
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the pattern Generator and number Analyzer!
+
+Select an option:
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a range of Number
+
+Enter your pattern: 1
+Enter the number of rows for the pattern: 4
+Pattern:
+*
+**
+***
+****
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the pattern Generator and number Analyzer!
+
+Select an option:
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a range of Number
+
+Enter your pattern: 1
+Enter the number of rows for the pattern: 5
+Pattern:
+*
+**
+***
+****
+*****
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the Pattern Generator and Number Analyzer!
+
+Select an option:
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a Range of Numbers
+Enter your choice: 1
+Enter the start of the range: 15
+Enter the end of the range: 455  0
+Traceback (most recent call last):
+  File "C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py", line 13, in <module>
+    end = int(input("Enter the end of the range: "))
+ValueError: invalid literal for int() with base 10: '455  0'
+
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the Pattern Generator and Number Analyzer!
+
+Select an option:
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a Range of Numbers
+Enter your choice: 
+=============== RESTART: C:\Users\R31\AppData\Local\Programs\Python\Python313\project_1.2 .py ===============
+Welcome to the Pattern Generator and Number Analyzer!
+
+Select an option:
+1. Right-angled Triangle
+2. Pyramid
+3. Left-angled Triangle
+4. Analyze a Range of Numbers
+Enter your choice: 1
+Enter the start of the range: 5
+Enter the end of the range: 500
+
+Number 5 is Odd
+Number 6 is Even
+Number 7 is Odd
+Number 8 is Even
+Number 9 is Odd
+Number 10 is Even
+Number 11 is Odd
+Number 12 is Even
+Number 13 is Odd
+Number 14 is Even
+Number 15 is Odd
+Number 16 is Even
+Number 17 is Odd
+Number 18 is Even
+Number 19 is Odd
+Number 20 is Even
+Number 21 is Odd
+Number 22 is Even
+Number 23 is Odd
+Number 24 is Even
+Number 25 is Odd
+Number 26 is Even
+Number 27 is Odd
+Number 28 is Even
+Number 29 is Odd
+Number 30 is Even
+Number 31 is Odd
+Number 32 is Even
+Number 33 is Odd
+Number 34 is Even
+Number 35 is Odd
+Number 36 is Even
+Number 37 is Odd
+Number 38 is Even
+Number 39 is Odd
+Number 40 is Even
+Number 41 is Odd
+Number 42 is Even
+Number 43 is Odd
+Number 44 is Even
+Number 45 is Odd
+Number 46 is Even
+Number 47 is Odd
+Number 48 is Even
+Number 49 is Odd
+Number 50 is Even
+Number 51 is Odd
+Number 52 is Even
+Number 53 is Odd
+Number 54 is Even
+Number 55 is Odd
+Number 56 is Even
+Number 57 is Odd
+Number 58 is Even
+Number 59 is Odd
+Number 60 is Even
+Number 61 is Odd
+Number 62 is Even
+Number 63 is Odd
+Number 64 is Even
+Number 65 is Odd
+Number 66 is Even
+Number 67 is Odd
+Number 68 is Even
+Number 69 is Odd
+Number 70 is Even
+Number 71 is Odd
+Number 72 is Even
+Number 73 is Odd
+Number 74 is Even
+Number 75 is Odd
+Number 76 is Even
+Number 77 is Odd
+Number 78 is Even
+Number 79 is Odd
+Number 80 is Even
+Number 81 is Odd
+Number 82 is Even
+Number 83 is Odd
+Number 84 is Even
+Number 85 is Odd
+Number 86 is Even
+Number 87 is Odd
+Number 88 is Even
+Number 89 is Odd
+Number 90 is Even
+Number 91 is Odd
+Number 92 is Even
+Number 93 is Odd
+Number 94 is Even
+Number 95 is Odd
+Number 96 is Even
+Number 97 is Odd
+Number 98 is Even
+Number 99 is Odd
+Number 100 is Even
+Number 101 is Odd
+Number 102 is Even
+Number 103 is Odd
+Number 104 is Even
+Number 105 is Odd
+Number 106 is Even
+Number 107 is Odd
+Number 108 is Even
+Number 109 is Odd
+Number 110 is Even
+Number 111 is Odd
+Number 112 is Even
+Number 113 is Odd
+Number 114 is Even
+Number 115 is Odd
+Number 116 is Even
+Number 117 is Odd
+Number 118 is Even
+Number 119 is Odd
+Number 120 is Even
+Number 121 is Odd
+Number 122 is Even
+Number 123 is Odd
+Number 124 is Even
+Number 125 is Odd
+Number 126 is Even
+Number 127 is Odd
+Number 128 is Even
+Number 129 is Odd
+Number 130 is Even
+Number 131 is Odd
+Number 132 is Even
+Number 133 is Odd
+Number 134 is Even
+Number 135 is Odd
+Number 136 is Even
+Number 137 is Odd
+Number 138 is Even
+Number 139 is Odd
+Number 140 is Even
+Number 141 is Odd
+Number 142 is Even
+Number 143 is Odd
+Number 144 is Even
+Number 145 is Odd
+Number 146 is Even
+Number 147 is Odd
+Number 148 is Even
+Number 149 is Odd
+Number 150 is Even
+Number 151 is Odd
+Number 152 is Even
+Number 153 is Odd
+Number 154 is Even
+Number 155 is Odd
+Number 156 is Even
+Number 157 is Odd
+Number 158 is Even
+Number 159 is Odd
+Number 160 is Even
+Number 161 is Odd
+Number 162 is Even
+Number 163 is Odd
+Number 164 is Even
+Number 165 is Odd
+Number 166 is Even
+Number 167 is Odd
+Number 168 is Even
+Number 169 is Odd
+Number 170 is Even
+Number 171 is Odd
+Number 172 is Even
+Number 173 is Odd
+Number 174 is Even
+Number 175 is Odd
+Number 176 is Even
+Number 177 is Odd
+Number 178 is Even
+Number 179 is Odd
+Number 180 is Even
+Number 181 is Odd
+Number 182 is Even
+Number 183 is Odd
+Number 184 is Even
+Number 185 is Odd
+Number 186 is Even
+Number 187 is Odd
+Number 188 is Even
+Number 189 is Odd
+Number 190 is Even
+Number 191 is Odd
+Number 192 is Even
+Number 193 is Odd
+Number 194 is Even
+Number 195 is Odd
+Number 196 is Even
+Number 197 is Odd
+Number 198 is Even
+Number 199 is Odd
+Number 200 is Even
+Number 201 is Odd
+Number 202 is Even
+Number 203 is Odd
+Number 204 is Even
+Number 205 is Odd
+Number 206 is Even
+Number 207 is Odd
+Number 208 is Even
+Number 209 is Odd
+Number 210 is Even
+Number 211 is Odd
+Number 212 is Even
+Number 213 is Odd
+Number 214 is Even
+Number 215 is Odd
+Number 216 is Even
+Number 217 is Odd
+Number 218 is Even
+Number 219 is Odd
+Number 220 is Even
+Number 221 is Odd
+Number 222 is Even
+Number 223 is Odd
+Number 224 is Even
+Number 225 is Odd
+Number 226 is Even
+Number 227 is Odd
+Number 228 is Even
+Number 229 is Odd
+Number 230 is Even
+Number 231 is Odd
+Number 232 is Even
+Number 233 is Odd
+Number 234 is Even
+Number 235 is Odd
+Number 236 is Even
+Number 237 is Odd
+Number 238 is Even
+Number 239 is Odd
+Number 240 is Even
+Number 241 is Odd
+Number 242 is Even
+Number 243 is Odd
+Number 244 is Even
+Number 245 is Odd
+Number 246 is Even
+Number 247 is Odd
+Number 248 is Even
+Number 249 is Odd
+Number 250 is Even
+Number 251 is Odd
+Number 252 is Even
+Number 253 is Odd
+Number 254 is Even
+Number 255 is Odd
+Number 256 is Even
+Number 257 is Odd
+Number 258 is Even
+Number 259 is Odd
+Number 260 is Even
+Number 261 is Odd
+Number 262 is Even
+Number 263 is Odd
+Number 264 is Even
+Number 265 is Odd
+Number 266 is Even
+Number 267 is Odd
+Number 268 is Even
+Number 269 is Odd
+Number 270 is Even
+Number 271 is Odd
+Number 272 is Even
+Number 273 is Odd
+Number 274 is Even
+Number 275 is Odd
+Number 276 is Even
+Number 277 is Odd
+Number 278 is Even
+Number 279 is Odd
+Number 280 is Even
+Number 281 is Odd
+Number 282 is Even
+Number 283 is Odd
+Number 284 is Even
+Number 285 is Odd
+Number 286 is Even
+Number 287 is Odd
+Number 288 is Even
+Number 289 is Odd
+Number 290 is Even
+Number 291 is Odd
+Number 292 is Even
+Number 293 is Odd
+Number 294 is Even
+Number 295 is Odd
+Number 296 is Even
+Number 297 is Odd
+Number 298 is Even
+Number 299 is Odd
+Number 300 is Even
+Number 301 is Odd
+Number 302 is Even
+Number 303 is Odd
+Number 304 is Even
+Number 305 is Odd
+Number 306 is Even
+Number 307 is Odd
+Number 308 is Even
+Number 309 is Odd
+Number 310 is Even
+Number 311 is Odd
+Number 312 is Even
+Number 313 is Odd
+Number 314 is Even
+Number 315 is Odd
+Number 316 is Even
+Number 317 is Odd
+Number 318 is Even
+Number 319 is Odd
+Number 320 is Even
+Number 321 is Odd
+Number 322 is Even
+Number 323 is Odd
+Number 324 is Even
+Number 325 is Odd
+Number 326 is Even
+Number 327 is Odd
+Number 328 is Even
+Number 329 is Odd
+Number 330 is Even
+Number 331 is Odd
+Number 332 is Even
+Number 333 is Odd
+Number 334 is Even
+Number 335 is Odd
+Number 336 is Even
+Number 337 is Odd
+Number 338 is Even
+Number 339 is Odd
+Number 340 is Even
+Number 341 is Odd
+Number 342 is Even
+Number 343 is Odd
+Number 344 is Even
+Number 345 is Odd
+Number 346 is Even
+Number 347 is Odd
+Number 348 is Even
+Number 349 is Odd
+Number 350 is Even
+Number 351 is Odd
+Number 352 is Even
+Number 353 is Odd
+Number 354 is Even
+Number 355 is Odd
+Number 356 is Even
+Number 357 is Odd
+Number 358 is Even
+Number 359 is Odd
+Number 360 is Even
+Number 361 is Odd
+Number 362 is Even
+Number 363 is Odd
+Number 364 is Even
+Number 365 is Odd
+Number 366 is Even
+Number 367 is Odd
+Number 368 is Even
+Number 369 is Odd
+Number 370 is Even
+Number 371 is Odd
+Number 372 is Even
+Number 373 is Odd
+Number 374 is Even
+Number 375 is Odd
+Number 376 is Even
+Number 377 is Odd
+Number 378 is Even
+Number 379 is Odd
+Number 380 is Even
+Number 381 is Odd
+Number 382 is Even
+Number 383 is Odd
+Number 384 is Even
+Number 385 is Odd
+Number 386 is Even
+Number 387 is Odd
+Number 388 is Even
+Number 389 is Odd
+Number 390 is Even
+Number 391 is Odd
+Number 392 is Even
+Number 393 is Odd
+Number 394 is Even
+Number 395 is Odd
+Number 396 is Even
+Number 397 is Odd
+Number 398 is Even
+Number 399 is Odd
+Number 400 is Even
+Number 401 is Odd
+Number 402 is Even
+Number 403 is Odd
+Number 404 is Even
+Number 405 is Odd
+Number 406 is Even
+Number 407 is Odd
+Number 408 is Even
+Number 409 is Odd
+Number 410 is Even
+Number 411 is Odd
+Number 412 is Even
+Number 413 is Odd
+Number 414 is Even
+Number 415 is Odd
+Number 416 is Even
+Number 417 is Odd
+Number 418 is Even
+Number 419 is Odd
+Number 420 is Even
+Number 421 is Odd
+Number 422 is Even
+Number 423 is Odd
+Number 424 is Even
+Number 425 is Odd
+Number 426 is Even
+Number 427 is Odd
+Number 428 is Even
+Number 429 is Odd
+Number 430 is Even
+Number 431 is Odd
+Number 432 is Even
+Number 433 is Odd
+Number 434 is Even
+Number 435 is Odd
+Number 436 is Even
+Number 437 is Odd
+Number 438 is Even
+Number 439 is Odd
+Number 440 is Even
+Number 441 is Odd
+Number 442 is Even
+Number 443 is Odd
+Number 444 is Even
+Number 445 is Odd
+Number 446 is Even
+Number 447 is Odd
+Number 448 is Even
+Number 449 is Odd
+Number 450 is Even
+Number 451 is Odd
+Number 452 is Even
+Number 453 is Odd
+Number 454 is Even
+Number 455 is Odd
+Number 456 is Even
+Number 457 is Odd
+Number 458 is Even
+Number 459 is Odd
+Number 460 is Even
+Number 461 is Odd
+Number 462 is Even
+Number 463 is Odd
+Number 464 is Even
+Number 465 is Odd
+Number 466 is Even
+Number 467 is Odd
+Number 468 is Even
+Number 469 is Odd
+Number 470 is Even
+Number 471 is Odd
+Number 472 is Even
+Number 473 is Odd
+Number 474 is Even
+Number 475 is Odd
+Number 476 is Even
+Number 477 is Odd
+Number 478 is Even
+Number 479 is Odd
+Number 480 is Even
+Number 481 is Odd
+Number 482 is Even
+Number 483 is Odd
+Number 484 is Even
+Number 485 is Odd
+Number 486 is Even
+Number 487 is Odd
+Number 488 is Even
+Number 489 is Odd
+Number 490 is Even
+Number 491 is Odd
+Number 492 is Even
+Number 493 is Odd
+Number 494 is Even
+Number 495 is Odd
+Number 496 is Even
+Number 497 is Odd
+Number 498 is Even
+Number 499 is Odd
+Number 500 is Even
+Sum of all numbers from 5 to 500 is: 125240
